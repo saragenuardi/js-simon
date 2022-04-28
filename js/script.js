97ce4cf7f4cef4a92b72704ce20c9b2f7943771b
saragenuardi <saretta.summer16@gmail.com>
2022-04-28 16:24:36 +0200

    new commit

--- js/script.js
+++ js/script.js
@@ -8,31 +8,60 @@
 const numberRandom = [];
 
 //Genera un numero causale per 5 volte con ciclo FOR
-for (let index = 0; index < 5; index++) {
+for (let index = 1; index <= 5; index++) {
     const singleNumber = getRndInteger(1, 100);
-    numberRandom.push(singleNumber); 
+    numberRandom.push(singleNumber);  //WHILE
 }
 
 //Visualizza i numeri casuali su HTML
-document. getElementById("numero-casuale").innerHTML = "I 5 numeri casuali sono " + numberRandom;
+document.getElementById("numero-casuale").innerHTML = "I 5 numeri casuali sono " + numberRandom;
 
 //Genera un timer di 30 secondi
 const countTimer = document.getElementById("timer");
-let count  = parseInt(countTimer.textContent);
-console.log(countTimer)
+let count = 5;
  
 const timer = setInterval(function() {
     count--;
-    if(count === 0) {
+    countTimer.innerHTML = "Restano " + count + " secondi";
+
+    if (count === 0) {
+        // nascondere i numeri
+        document.getElementById("numero-casuale").innerHTML = "";
+
+
+        const userNumberSave = []
+        //Chiedi all'utente, di inserire uno alla volta, i numeri random generati  precendentemente, tramite il prompt
+        for (let index = 1; index <= 5; index++) {
+            const userNumber = prompt("Inserisci il numero visualizzato precendetemente");
+            //Salva i numeri inseriti dall'utente dentro l'array
+            userNumberSave.push(userNumber);
+            console.log(userNumberSave);
+            //Controlla se i numeri inseriti dall'utente sono stati indovinati, e quindi uguali, ai numeri generati random dal PC
+            checkNumbers(userNumberSave)
+
+         }
+         
+        
+       
+
+        // quanti numeri ha indovinato?
+
+        
         clearInterval(timer);
-        countTimer.innerHTML = "Al timer restano"
     }
 }, 1000);
 
-//Chiedi all'utente, di insserire uno alla volta, i numeri random generati  precendentemente, tramite il prompt
-
 
 //FUNCTIONS
+
+function checkNumbers(userNumbers) {
+    
+
+}
+
+
+
+
 
 //Funzione per generare numeri random 
 function getRndInteger(min, max) {
